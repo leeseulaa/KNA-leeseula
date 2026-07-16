e1471d2d9c4f4c746f9a56785f5ec9f8c9e5f7b6
leeseulaa <leeseula5651@gmail.com>
2026-07-16 12:28:14 +0900

    study: 객체

---
+++ 05_obiect_prac.py
@@ -0,0 +1,28 @@
+# count = 3 #int
+# temp = 1.5 #float
+# str = "센서 A"
+# is_ok = True # bool
+# print(count, temp, str, is_ok)
+# print(type(5)) # <class 'int'>
+# print(type(36.5)) #  <class 'float'>
+# print(type("센서 A")) #  <class 'str'>
+# print(type(3 > 1)) #  <class 'bool'>
+# print(type(100)) #  <class 'int'>
+# print(type(100.0)) #  <class 'float'>
+# print(type("100")) #  <class 'str'>
+# print( 5 + 5) # 10
+# print("3"+"4") # 34
+# print("30"+"40") # 3040
+# print( 3 > 2) # True 
+# print( 5 == 5) # Ture
+# print(type(3 > 2)) # <class "bool"> 
+# count = 5 
+# print(count, type(count)) # <class 'int'>
+# count = 5.0 
+# print(count, type(count))  # <class 'float'>
+# count = "5"
+# print(count, type(count))  #  <class 'str'>
+device_temp = 25.5 # <class 'float'>
+device_count = 30 # <class 'int'>
+device_name = "센서 A" # <class 'str'>
+is_normal = True # <class 'bool'>
